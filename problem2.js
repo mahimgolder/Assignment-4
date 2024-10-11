bc3bb93d729865d013c2f86d124ba22a07faec08
mahimgolder <mahimgolder@gmail.com>
2024-10-11 09:56:36 -0600

    created sentNotificationMassage function with slice method done

--- problem2.js
+++ problem2.js
@@ -1,3 +1,4 @@
+//****************** split use kore :
 function sentNotification(email){
     const index = email.indexOf("@");
     if(index === -1){
@@ -9,10 +10,29 @@ function sentNotification(email){
     const notificationMassage = userName + " sent you an email from " + domainName ;
     return notificationMassage;
 }
-console.log(sentNotification("zihad@gmail.com"));
-console.log(sentNotification("farhan34@yahoo.com"));
-console.log(sentNotification("nadim.naem5@outlook.com"));
-console.log(sentNotification("fahim234.hotmail.com"));
-console.log(sentNotification("sadia8icloud.com"));
+// console.log(sentNotification("zihad@gmail.com"));
+// console.log(sentNotification("farhan34@yahoo.com"));
+// console.log(sentNotification("nadim.naem5@outlook.com"));
+// console.log(sentNotification("fahim234.hotmail.com"));
+// console.log(sentNotification("sadia8icloud.com"));
+
+
+// ************slice method use kore:
+function sentNotification(email){
+    if(email.indexOf("@") === -1){
+        return "Invalid Email";
+    }
+    const atIndex = email.indexOf("@");
+    const userName = email.slice(0, atIndex);
+    const domainName = email.slice(atIndex + 1);
+    const notifacationMassage = userName + " sent you an email from " + domainName;
+    return notifacationMassage
+
+}
+// console.log(sentNotification("zihad@gmail.com"));
+// console.log(sentNotification("farhan34@yahoo.com"));
+// console.log(sentNotification("nadim.naem5@outlook.com"));
+// console.log(sentNotification("fahim234.hotmail.com"));
+// console.log(sentNotification("sadia8icloud.com"));
 
 
