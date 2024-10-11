8fcad20ac2e35bb6a80a82b7c99c9e080892a1c8
mahimgolder <mahimgolder@gmail.com>
2024-10-10 20:12:01 -0600

    calculateTax fuction done

---
+++ problem1.js
@@ -0,0 +1,15 @@
+function calculateTax(income, expenses ) {
+    if(income < 0 || expenses < 0 || income < expenses){
+        return "Invalid Input";
+    }
+    const difference = income - expenses;
+    const tax = difference * 0.20;
+    return tax;
+    
+}
+console.log(calculateTax(10000, 3000));
+console.log(calculateTax(34000, 1753));
+console.log(calculateTax(5000, 1500));
+console.log(calculateTax(7000, 2000));
+console.log(calculateTax(-5000, 2000));
+console.log(calculateTax(6000, -1500));
